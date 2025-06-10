4d21e68a72321a6e0a122f29afb25f73b64ffe3a
Aaryan Pradhan <pradhanaaryan@gmail.com>
2025-06-10 17:35:25 +0530

    Add user authentication and link tasks to user

--- tasks/views.py
+++ tasks/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
 from .models import Task
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def task_list(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         if title:
-            Task.objects.create(title=title)
-            return redirect('/')  # refresh after submit
+            Task.objects.create(title=title, user=request.user)
+            return redirect('/')
 
-    tasks = Task.objects.all()
+    tasks = Task.objects.filter(user=request.user)
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
 
 def complete_task(request, task_id):
